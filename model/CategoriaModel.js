13ce61c711668878b212673f9242c6cab382ba25
Genivania Macedo <109962733+Genivania@users.noreply.github.com>
2023-09-01 10:01:29 -0300

    alo

---
+++ model/CategoriaModel.js
@@ -0,0 +1,24 @@
+
+//DESCRICAO DA TABELA DO BANCO DE DADOS
+const Sequelize = require('sequelize');
+
+const connection = require('../database/database');
+
+const Categoria = connection.define(
+    'tbl_categoria',
+    {
+        codigo_categoria:{
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        nome_categoria:{
+        type: Sequelize.STRING,
+        allowNull: false
+        }
+    }
+);
+
+Categoria.sync({force:false});
+
+module.exports = Categoria;
